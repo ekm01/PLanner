e8640ab4764f2232885136ce8383e41934689be8
ekm01 <ekm7p8j@gmail.com>
2023-04-25 17:37:17 +0200

    added a version that prints calendar day by day

--- planner.py
+++ planner.py
@@ -7,8 +7,20 @@ from datetime import datetime, time, timedelta
 import pytz
 import sys
 
+
 class WrongArgumentException(Exception):
     pass
+
+
+def printEvents(events, day):
+    print('\n' + day + ':')
+    if not events:
+        print('No events found.')
+    else:
+        for i in range(0, len(events)):
+            event_start = events[i]['start'].get('dateTime', events[i]['start'].get('date'))
+            event_start = datetime.fromisoformat(event_start).strftime('%I:%M %p')
+            print(str(i + 1) + '. ' + events[i]['summary'] + ' at ' + event_start)
 
 
 def getCalendar(secretsPath, timezone, choice):
@@ -16,18 +28,19 @@ def getCalendar(secretsPath, timezone, choice):
 
     # setting the start time, end time is relative to start
     today = datetime.today()
-    start = datetime.combine(today, time.min).astimezone(tz)  
-    
+    start = datetime.combine(today, time.min).astimezone(tz)
+    loopEnd = 0
+
     if choice == 'rest':
-        end = start + timedelta(days=7-today.date().weekday())
+        loopEnd = 7 - today.date().weekday()
     elif choice == 'week':
         start = start - timedelta(days=today.date().weekday())
-        end = start + timedelta(days=7)
+        loopEnd = 7
     elif choice == 'nextweek':
         start = start + timedelta(days=7-today.date().weekday())
-        end = start + timedelta(days=7)
+        loopEnd = 7
     elif choice == 'today':
-        end = start + timedelta(days=1)
+        loopEnd = 1
     else:
         raise WrongArgumentException('Wrong input for the second parameter.')
 
@@ -41,39 +54,37 @@ def getCalendar(secretsPath, timezone, choice):
     # create an instance of a google api client
     cal = build('calendar', 'v3', credentials=creds)
 
-    # fetching the events
-    events = cal.events().list(calendarId='ekm7p8j@gmail.com',
-                               timeMin=start.isoformat(),
-                               timeMax=end.isoformat(),
-                               singleEvents=True,
-                               timeZone=timezone,
-                               orderBy='startTime').execute()
+    # printing the events day by day
+    for i in range(0, loopEnd):
+        startl = start + timedelta(days=i)
+        endl = startl + timedelta(days=1)
 
-    # getting a list of events
-    return events.get('items', [])
+        # fetching the events
+        events = cal.events().list(calendarId='ekm7p8j@gmail.com',
+                                   timeMin=startl.isoformat(),
+                                   timeMax=endl.isoformat(),
+                                   singleEvents=True,
+                                   timeZone=timezone,
+                                   orderBy='startTime').execute()
 
-
-def printEvents(events):
-    if not events:
-        print('No events found.')
-    else:
-        for i in range(0, len(events)):
-            event_start = events[i]['start'].get('dateTime', events[i]['start'].get('date'))
-            event_start = datetime.fromisoformat(event_start).strftime('%I:%M %p')
-            print(str(i + 1) + '. ' + events[i]['summary'] + ' at ' + event_start)
+        # getting a list of events
+        events = events.get('items', [])
+        printEvents(events, startl.strftime('%A, %d-%m-%Y'))
 
 
 # getting the program arguments
-args = sys.argv
+def main():
+    args = sys.argv
 
-if len(args) != 3:
-    print('Invalid number of parameters: expected 2')
-    sys.exit(1)
+    if len(args) != 3:
+        print('Invalid number of parameters: expected 2')
+        sys.exit(1)
 
-try:
-    events = getCalendar('client_secret.json', args[1], args[2])
-except WrongArgumentException as e:
-    print(e)
-    sys.exit(1)
+    try:
+        getCalendar('client_secret.json', args[1], args[2])
+    except WrongArgumentException as e:
+        print(e)
+        sys.exit(1)
 
-printEvents(events)
+
+main()
